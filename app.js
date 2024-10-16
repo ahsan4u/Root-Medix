7bab62ea1fbe11ab255e435fd20a00a684827b3b
Mohd Ahsan <mohdahsanahsan1@gmail.com>
2024-10-16 20:15:41 +0530

    Update app.js. Added env config

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const nodeMailer = require('nodemailer');
+env.config() = required('env');
 
 const app = express();
 
