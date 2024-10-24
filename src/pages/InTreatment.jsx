bdaa4aa9cd070e8ce663c51a26560555e8ae9f01
Mohd Ahsan <mohdahsanahsan1@gmail.com>
2024-10-24 17:38:12 +0530

    fix some error

--- src/pages/InTreatment.jsx
+++ src/pages/InTreatment.jsx
@@ -16,7 +16,7 @@ function InTreatment({name}) {
                     {
                         items.map((item, idx)=>(
                             <Link key={idx} to="/treatment/cardiology/heart-transplant" className='sm:w-[85%] w-[95%] hover:scale-105 transition-all duration-500'>
-                            <div className='flex aspect-[16/10] rounded-2xl drop-shadow-xl overflow-hidden mb-6 relative'>
+                            <div className='flex aspect-[16/10] rounded-2xl drop-shadow-xl overflow-hidden lg:mb-6 mb-3 relative'>
                                 <div className='text-white w-[65%] flex flex-col justify-between pl-2 py-2 h-full bg-[url("/img/treatment-card.png")] bg-[length:100%_100%] z-20 rounded-l-2xl'>
                                     <h2 className='font-bold text-2xl lg:text-3xl'>{item.name}</h2>
                                     <p className='text-xs w-[60%]'>{item.info}</p>
