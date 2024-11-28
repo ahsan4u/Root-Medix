ec5a4c0168967921d2248074115f13f40fb61d73
Mohd Ahsan <mohdahsanahsan1@gmail.com>
2024-11-28 15:45:00 +0530

    with import system

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,8 @@ import TreatmentCard from "@/components/TreatmentCard";
 import HospitalCard from "@/components/HospitalCard";
 import DoctorCard from "@/components/DoctorCard";
 import ServiceCard from "@/components/ServiceCard";
+import data from "../data/cards";
 export default async function Home() {
-  const host = process.env.NEXT_PUBLIC_HOST || 'http://localhost:3000';
-  const data = await (await fetch(`${host}/data/cards.json`)).json();
   const bestTreatments = data['treatments'];
   const bestHospitals = data['hospitals'];
   const services = data['services'];
@@ -21,15 +20,15 @@ export default async function Home() {
     </div>
 
     <div className="mt-1 mb-6 lg:mt-10 lg:mb-14">
-      <ScrollDiv heading={'Best Hospitals'} cardsData={bestHospitals} Card={HospitalCard} count={4}/>
+      {bestHospitals && (<ScrollDiv heading={'Best Hospitals'} cardsData={bestHospitals} Card={HospitalCard} count={4}/>)}
     </div>
 
     <div className="mt-1 mb-6 lg:mt-10 lg:mb-14">
-      <ScrollDiv heading={'Our Services'} cardsData={services} Card={ServiceCard} count={4}/>
+      {services && (<ScrollDiv heading={'Our Services'} cardsData={services} Card={ServiceCard} count={4}/>)}
     </div>
 
     <div className="mt-1 mb-6 lg:mt-10 lg:mb-14">
-      <ScrollDiv heading={'Best Doctors'} cardsData={bestDoctors} Card={DoctorCard} link={'/doctors'} count={4} seeMore={true} scroll={true}/>
+      {bestDoctors && (<ScrollDiv heading={'Best Doctors'} cardsData={bestDoctors} Card={DoctorCard} link={'/doctors'} count={4} seeMore={true} scroll={true}/>)}
     </div>
 
   </>);
--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { useState, useRef } from "react";
 import Massage from "./massage";
-import STDcode from "../../public/data/STDcode";
+import STDcode from "../data/STDcode";
 import Lottie from 'lottie-react';
 import loadingEffect from '../../public/animated Icon/loading.json';
 
